ace6a552b3b4d4d48bae5ab68d58f017c1d91ce4
shashikant <shashikant.ghule@gmail.com>
2019-10-07 23:01:01 +0530

    added some thing

--- loops.py
+++ loops.py
@@ -8,3 +8,28 @@ for i in range(1,10):
 	if i < 5:
 		print(i)
 >>>>>>> 337f43ca80529774db072561ca296af8ebb649b3
+
+
+commit 923c0caf1355dd051e60605df735145284db2e43 (HEAD -> master, origin/master)
+Merge: 9fe2913 337f43c
+Author: shashikant <shashikant.ghule@gmail.com>
+Date:   Mon Oct 7 22:52:33 2019 +0530
+
+    just file
+
+commit 9fe2913bce8fd19eeb607b6d9a68b43a82236045
+Author: shashikant <shashikant.ghule@gmail.com>
+Date:   Mon Oct 7 22:39:38 2019 +0530
+
+    just
+
+commit 337f43ca80529774db072561ca296af8ebb649b3
+Author: shashikantghule <44537695+shashikantghule@users.noreply.github.com>
+Date:   Mon Oct 7 22:38:18 2019 +0530
+
+    Update loop
+    
+    update loop with if statement.
+
+commit 8878d82d6d80fdddf1cc24782c184f97a61dcfa4
+Author: shashikant <shashikant.ghule@gmail.com>
